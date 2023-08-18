6676efc0a728fae10de78e470f2ed761cdcef425
ITTIPOL_CHA <110074014+ITTIPOLCHA@users.noreply.github.com>
2023-08-18 13:56:02 +0700

    Add_banner
    Add banner

--- src/App.js
+++ src/App.js
@@ -4,34 +4,10 @@ import { Header, Content, Footer } from "./func-components";
 import Calendar from "./class-component";
 import MsgBox from "./class-props";
 import MsgBox2 from "./func-props";
+import Banner from "./banner";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <p>
-        <center>
-          <Calendar />
-        </center>
-      </p>
-      <Content />
-      <MsgBox
-        text="React & React Native"
-        color="blue"
-        bgColor="#ccc"
-        fontSize="18pt"
-        border="solid 1px black"
-      />
-      <MsgBox2
-        text="การใช้ props ใน Function Component"
-        color="red"
-        bgColor="#ccc"
-        fontSize="18pt"
-        border="solid 1px black"
-      />
-      <Footer />
-    </>
-  );
+  return <Banner />;
 }
 
 export default App;
