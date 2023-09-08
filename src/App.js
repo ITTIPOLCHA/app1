0e5961b016dd007739d669246c73eb5bf4d9e1b8
ITTIPOL_CHA <110074014+ITTIPOLCHA@users.noreply.github.com>
2023-09-08 15:11:13 +0700

    context_content
    context_content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { userContext } from "./context";
 import Header from "./context-header";
+import Content from "./context-content";
 
 function App() {
   return (
     <userContext.Provider value={"Tom Jerry"}>
       <Header />
+      <Content />
     </userContext.Provider>
   );
 }
