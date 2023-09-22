edd19b0274559aa32cce6e71ec396f038f0c00bd
ITTIPOLCHA <110074014+ITTIPOLCHA@users.noreply.github.com>
2023-09-22 14:32:55 +0700

    Add route3

---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function home() {
+  return (
+    <p style={{ textAlign: "center" }}>
+      <h3>ยินดีต้อนรับสู่ React Store</h3>
+      รายชื่อ <a href="/products">สินค้า</a> ที่เรามีจำหน่าย
+      <br />
+      เเต่ท่านต้องเป็น <a href="/member">สมาชิก</a> จึงจะ สั่งซื้อได้
+      <br />
+      หากมีข้อสงสัย <a href="/contact">ติดต่อ</a>
+    </p>
+  );
+}
---
+++ src/member.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Member extends Component {
+  render() {
+    return <div style={{ textAlign: "center" }}>Member Page</div>;
+  }
+}
